c89e23e46983352cc239a5fff95531aa3f554b95
suhongkim98 <kshz9815@naver.com>
2022-04-21 17:51:25 +0900

    init commit

---
+++ src/main/java/com/soma/lightening/LighteningApplication.java
@@ -0,0 +1,13 @@
+package com.soma.lightening;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LighteningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LighteningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soma/lightening/LighteningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soma.lightening;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LighteningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
