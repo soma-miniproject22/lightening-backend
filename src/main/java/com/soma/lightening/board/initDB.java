aef75401e83034e6365823f15870cbbdfd3adbca
Lee SangBin(MUMOMU) <rhkd2612@naver.com>
2022-04-26 14:51:46 +0900

    lastest git test
    lastest git test

--- src/main/java/com/soma/lightening/board/initDB.java
+++ src/main/java/com/soma/lightening/board/initDB.java
@@ -11,26 +11,26 @@ import javax.persistence.EntityManager;
 @Component
 @RequiredArgsConstructor
 public class initDB {
-    private final InitService initService;
-
-    @PostConstruct
-    public void init() {
-        System.out.println("init db progress");
-        initService.dbInit1();
-    }
-
-    @Component
-    @Transactional
-    @RequiredArgsConstructor
-    static class InitService {
-        private final EntityManager em;
-
-        public void dbInit1() {
+//    private final InitService initService;
+//
+//    @PostConstruct
+//    public void init() {
+//        System.out.println("init db progress");
+//        initService.dbInit1();
+//    }
+//
+//    @Component
+//    @Transactional
+//    @RequiredArgsConstructor
+//    static class InitService {
+//        private final EntityManager em;
+//
+//        public void dbInit1() {
 //            OAuth2Account account1 =
 //
 //            Post post1 = createPost();
 //            em.persist(post);
-        }
+//        }
 
 //        private OAuth2Account createAccount(String name) {
 //            OAuth2Account account = new OAuth2Account(name);
@@ -42,5 +42,5 @@ public class initDB {
 //
 //            return post;
 //        }
-    }
+//    }
 }
