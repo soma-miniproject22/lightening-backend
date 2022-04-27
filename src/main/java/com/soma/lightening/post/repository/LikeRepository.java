0efa17d2ab99bb1db843bc9794bba7293fa9524c
Lee SangBin(MUMOMU) <rhkd2612@naver.com>
2022-04-28 00:52:43 +0900

    Delete application.yml
    Like Entity 설계 및 Post API 구현
    
    Update Like.java

--- src/main/java/com/soma/lightening/post/repository/LikeRepository.java
+++ src/main/java/com/soma/lightening/post/repository/LikeRepository.java
@@ -5,9 +5,13 @@ import com.soma.lightening.post.domain.Like;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface LikeRepository extends JpaRepository<Like, Long> {
-    @EntityGraph(attributePaths = "authorities") // 엔티티그래프 통해 EAGER로 가져온다.
-    Optional<Like> findById(Long id);
+    @EntityGraph(attributePaths = "account") // 엔티티그래프 통해 EAGER로 가져온다.
+    public Optional<Like> findById(Long id);
+
+    @EntityGraph(attributePaths = "account")
+    public List<Like> findAllByAccount_Id(Long accountId);
 }
