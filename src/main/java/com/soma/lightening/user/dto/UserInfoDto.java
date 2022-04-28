925821fd7ea016616bba31cc62f2fca3b12b3a24
suhongkim98 <kshz9815@naver.com>
2022-04-28 23:03:22 +0900

    Getter 어노테이션 추가

--- src/main/java/com/soma/lightening/user/dto/UserInfoDto.java
+++ src/main/java/com/soma/lightening/user/dto/UserInfoDto.java
@@ -2,8 +2,10 @@ package com.soma.lightening.user.dto;
 
 import com.soma.lightening.common.entity.OAuth2Account;
 import lombok.Builder;
+import lombok.Getter;
 
 @Builder
+@Getter
 public class UserInfoDto {
     private String username;
     private String profileImage;
